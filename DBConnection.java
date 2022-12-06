b0a99c44f2c3c83f9d50288564cf76bee26246e2
Rafik SALMI <rs375iwill@gmail.com>
2022-12-06 16:54:23 +0100

    question 2 et 3

--- DBConnection.java
+++ DBConnection.java
@@ -3,18 +3,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	   
-		String BDD = "nomBD";
-		String url = "jdbc:mysql://localhost:3306/" + BDD;
-		String user = "root";
-		String passwd = "";
-	    private Connection conn;
+	    
+		static String BDD = "nomBD";
+		static String url = "jdbc:mysql://localhost:3306/" + BDD;
+		static String user = "root";
+		static String passwd = "";
+	    private static Connection conn;
+	    
+	    public static DBConnection singleconnection = new DBConnection();
 	    
         //you'll find above : connection informations
-	   
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
-		}
+	    
+	    private DBConnection(){}
+	    
+	    public static DBConnection getInstance() throws SQLException{
+	    	conn=DriverManager.getConnection(url, user,passwd);
+	    	return singleconnection;
+	    
+	    }
+	    
+	    
 
 	    
 	    public Connection getConn() {
